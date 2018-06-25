c075a9837183099cf8e69ed54d17b9780a6c4c44
nishant-qait <nishantagarwal@qainfotech.com>
2018-06-25 09:34:14 +0530

    Update pairsWithSumIsK.cpp

--- pairsWithSumIsK.cpp
+++ pairsWithSumIsK.cpp
@@ -6,11 +6,11 @@ int main() {
 	cin>>n>>k;
 	for(i=0;i<n;i++){
 	 cin>>b[i];
-	 a[abs(b[i]-k)]=1;
+	 a[(k-b[i])]=1;
 	}
 	for(i=0;i<n;i++){
 		if(a[b[i]]==1)
-		  cout<<b[i]<<","<<abs(b[i]-k)<<"  ";
+		  cout<<b[i]<<","<<(k-b[i])<<"  ";
 	   	  
 	}
 	return 0;
